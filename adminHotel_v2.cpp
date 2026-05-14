376741b010b654bb18ea0b47f34d69a54c4da0fb
tam-pr <a01648580@tec.mx>
2026-05-13 21:36:05 -0600

    working draft

--- adminHotel_v2.cpp
+++ adminHotel_v2.cpp
@@ -14,6 +14,8 @@ class Hotel{
 
     protected: 
         static vector <int> roomNumbers; 
+        int childTariff = 250; 
+        int adultTariff = 650; 
     
     public: 
 
@@ -36,7 +38,7 @@ class Hotel{
         }; 
 
 }; 
-class Room: public Hotel { 
+class Room: public Hotel {
     
     private: 
         int roomNumber; 
@@ -46,55 +48,70 @@ class Room: public Hotel {
 
         static int pointer; 
 
-    public:
+    public: 
+        string guestName; 
+        int numberAdults;
+        int numberChildren;
+
         int getroomNumber(){
-            if (pointer > roomNumbers.size()){
-                cout <<"HOTEL AT CAPACITY" <<endl;
-                roomNumber = -1;
+            if (pointer < roomNumbers.size()
+            ){
+                roomNumber = roomNumbers[pointer]; 
+                pointer++; 
+                return roomNumber; 
             }
-
-            roomNumber = roomNumbers[pointer];
-            pointer++;
-            cout <<"Room number: " << roomNumber <<endl;
-            return roomNumber;
-
-        }; 
+            else {
+                return -1; 
+            }; 
+        }
+        ; 
 
         void getisAvailable(){
-            if (roomNumber == -1){
-                isAvailable = false; 
-            }
-
-            if (occupiedRooms.size() == 0){
+            getroomNumber();
+            if (roomNumber != -1){ 
                 isAvailable = true; 
-                //cout <<"Room is available" <<endl;
-            }
-            else {
-                for (int i = 0; i < occupiedRooms.size(); i++){
+
+                for (int i =0; i < occupiedRooms.size(); i++){
                     if (roomNumber == occupiedRooms[i]){
                         isAvailable = false; 
-                        //cout <<"Room is occupied" <<endl;
+                        break; 
                     }
-
-                    else {
-                        isAvailable = true; 
-                        //cout <<"Room is available" <<endl;
-                    }; 
-
                 }; 
-            }; 
-            occupiedRooms.push_back(roomNumber); 
-        }; 
-
-        void roomStatus(){
-            getisAvailable();
-            if (isAvailable == true){
-                cout <<"Room is available" <<endl;
+            
             }
             else {
-                cout <<"Room is occupied" <<endl;
-            }
+                isAvailable = false;
         }; 
+
+        };
+
+        string roomStatus(){
+    
+            getisAvailable();
+            string roomState;
+            if (roomNumber ==0){
+                roomState = "HOTEL AT CAPACITY"; 
+            }
+            else if (isAvailable == true){
+                roomState   = "Available"; 
+            }
+            else {
+                roomState = "Occupied"; 
+            }; 
+            return roomState; 
+        };
+
+        void checkIn(){
+            string roomState =roomStatus();
+            if (roomState != "Available"){
+                cout <<" Cant process checkin rn, hotels fully booked :(" <<endl;
+            }
+            else {
+                cout <<"Guest name: " <<endl;
+                cin >> guestName;
+                cout << "The room " << roomNumber << " is " << roomState << " for the reservation under the guest " << guestName << endl;
+            }
+        };
 
 
 }; 
@@ -109,13 +126,10 @@ int main() {
             hotel1.settotalRooms();
             hotel1.setroomNumbers();
 
-        Room room1; 
-            room1.getroomNumber();
-            room1.roomStatus();
-        
-        Room room2; 
-            room2.getroomNumber();
-            room2.roomStatus();
+        Room room1;
+            room1.checkIn();
+        Room room2;
+            room2.checkIn();
 
     return 0;
     }; 
